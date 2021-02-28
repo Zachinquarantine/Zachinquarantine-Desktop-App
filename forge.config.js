14c422761262516d39c7155932264ff73073c987
Zachinquarantine <69423184+Zachinquarantine@users.noreply.github.com>
2021-02-27 20:49:40 -0500

    update

--- forge.config.js
+++ forge.config.js
@@ -1,1 +1,10 @@
-
+{
+    name: '@electron-forge/publisher-github',
+    config: {
+      repository: { https://github.com/Zachinquarantine/Zachinquarantine-Desktop-App/
+        owner: 'Zachinquarantine',
+        name: 'awesome-thing'
+      },
+      prerelease: true
+    }
+  }
